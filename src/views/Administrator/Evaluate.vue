b001e9c4dd1f715884314ac29be11477a4f98933
Richy_D <60642415+RQD-china@users.noreply.github.com>
2021-07-10 20:08:23 +0800

    界面优化
    界面优化：
    审核酒店界面优化了表格宽度；
    管理酒店界面优化了表格宽度，搜索栏简化；
    完成回复用户界面（假数据）。

--- src/views/Administrator/Evaluate.vue
+++ src/views/Administrator/Evaluate.vue
@@ -1,136 +1,140 @@
 <template>
-	<el-container style="height: 700px">
-		<el-header style="background-color: #2c3e50; color: white">
-			<back-nav />
-		</el-header>
-		<el-container style="height: 100%">
-			<el-aside width="200px" style="background-color: #545c64">
-				<SideNav />
-			</el-aside>
-			<el-main>
-				<el-card class="box-card">
-					<!--待审核酒店列表-->
-					<h3>待审核酒店列表</h3>
-					<el-table :data="tableData" border style="width: 100%">
-						<el-table-column prop="hotelID" label="酒店ID" width="120">
-						</el-table-column>
-						<el-table-column prop="hotelName" label="酒店名" width="120">
-						</el-table-column>
-						<el-table-column prop="hotelProvince" label="省份" width="120">
-						</el-table-column>
-						<el-table-column prop="hotelCity" label="城市" width="120">
-						</el-table-column>
-						<el-table-column prop="hotelRegion" label="区" width="120">
-						</el-table-column>
-						<el-table-column prop="hotelAddress" label="地址" width="600">
-						</el-table-column>
-						<el-table-column fixed="right" label="详情" width="100">
-							<template #default="scope">
-								<el-button @click="handleClick(scope.row)" type="text" size="small">查看
-								</el-button>
+	<el-scrollbar height="600px">
+		<el-container style="height: 700px">
+			<el-header style="background-color: #2c3e50; color: white">
+				<back-nav />
+			</el-header>
+			<el-container style="height: 100%">
+				<el-aside width="200px" style="background-color: #545c64">
+					<SideNav />
+				</el-aside>
+				<el-main>
+					<el-card class="box-card">
+						<!--待审核酒店列表-->
+						<h3>待审核酒店列表</h3>
+						<el-row justify="center">
+							<el-table :data="tableData" border style="width: 80%">
+								<el-table-column prop="hotelID" label="酒店ID" width="120">
+								</el-table-column>
+								<el-table-column prop="hotelName" label="酒店名" width="120">
+								</el-table-column>
+								<el-table-column prop="hotelProvince" label="省份" width="80">
+								</el-table-column>
+								<el-table-column prop="hotelCity" label="城市" width="80">
+								</el-table-column>
+								<el-table-column prop="hotelRegion" label="区" width="80">
+								</el-table-column>
+								<el-table-column prop="hotelAddress" label="地址" width="300">
+								</el-table-column>
+								<el-table-column fixed="right" label="详情" width="80" align="center">
+									<template #default="scope">
+										<el-button @click="handleClick(scope.row)" type="text" size="small">查看
+										</el-button>
+									</template>
+								</el-table-column>
+							</el-table>
+						</el-row>
+						<!--点击"查看"按钮，打开酒店详情页面-->
+						<el-dialog title="酒店详情" v-model="dialogAudit" width="30%">
+							<el-descriptions title="基本信息" class="margin-top" :column="2" :size="size" border>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-user-solid"></i>
+										酒店ID
+									</template>
+									{{detailedInfo.hotelID}}
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-user"></i>
+										酒店名
+									</template>
+									{{ detailedInfo.hotelName }}
+								</el-descriptions-item>
+							</el-descriptions>
+							<el-descriptions class="margin-top" :column="3" :size="size" border>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-map-location"></i>
+										省
+									</template>
+									{{detailedInfo.hotelProvince}}
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-location-information"></i>
+										市
+									</template>
+									{{ detailedInfo.hotelCity }}
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-location-outline"></i>
+										区
+									</template>
+									{{ detailedInfo.hotelRegion }}
+								</el-descriptions-item>
+							</el-descriptions>
+							<el-descriptions class="margin-top" :column="1" :size="size" border>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-place"></i>
+										详细地址
+									</template>
+									{{ detailedInfo.hotelAddress }}
+								</el-descriptions-item>
+							</el-descriptions>
+							<el-descriptions title="审核信息" class="margin-top" :column="1" :size="size" border>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-postcard"></i>
+										卫生许可证
+									</template>
+									{{ detailedInfo.healthLicense }}
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-postcard"></i>
+										消防检查合格证
+									</template>
+									{{ detailedInfo.fireInspectionCertificate }}
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-postcard"></i>
+										餐饮服务经营许可证
+									</template>
+									{{ detailedInfo.cateringServiceLicense }}
+								</el-descriptions-item>
+							</el-descriptions>
+							<el-form label-position="formLabelPosition" :model="form">
+								<el-row class="autocomplete">
+									<el-col :span="24">
+										<div class="sub-title">审核说明</div>
+										<el-autocomplete class="inline-input" v-model="dataDescrip"
+											v-bind:disabled="disabledDescrip" :fetch-suggestions="querySearch"
+											placeholder="请输入内容" @select="handleSelect">
+										</el-autocomplete>
+									</el-col>
+								</el-row>
+								<p></p>
+								<div class="sub-title">审核结果</div>
+								<el-select v-model="form.choice" placeholder="请选择审核结果" @change="auditSelect">
+									<el-option label="审核驳回" value=false></el-option>
+									<el-option label="审核通过" value=true></el-option>
+								</el-select>
+							</el-form>
+							<template #footer>
+								<span class="dialog-footer">
+									<el-button type="primary" @click="submitClick" size="small">提交</el-button>
+								</span>
 							</template>
-						</el-table-column>
-					</el-table>
-					<!--点击"查看"按钮，打开酒店详情页面-->
-					<el-dialog title="酒店详情" v-model="dialogAudit" width="30%">
-						<el-descriptions title="基本信息" class="margin-top" :column="2" :size="size" border>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-user-solid"></i>
-									酒店ID
-								</template>
-								{{detailedInfo.hotelID}}
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-user"></i>
-									酒店名
-								</template>
-								{{ detailedInfo.hotelName }}
-							</el-descriptions-item>
-						</el-descriptions>
-						<el-descriptions class="margin-top" :column="3" :size="size" border>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-map-location"></i>
-									省
-								</template>
-								{{detailedInfo.hotelProvince}}
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-location-information"></i>
-									市
-								</template>
-								{{ detailedInfo.hotelCity }}
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-location-outline"></i>
-									区
-								</template>
-								{{ detailedInfo.hotelRegion }}
-							</el-descriptions-item>
-						</el-descriptions>
-						<el-descriptions class="margin-top" :column="1" :size="size" border>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-place"></i>
-									详细地址
-								</template>
-								{{ detailedInfo.hotelAddress }}
-							</el-descriptions-item>
-						</el-descriptions>
-						<el-descriptions title="审核信息" class="margin-top" :column="1" :size="size" border>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-postcard"></i>
-									卫生许可证
-								</template>
-								{{ detailedInfo.healthLicense }}
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-postcard"></i>
-									消防检查合格证
-								</template>
-								{{ detailedInfo.fireInspectionCertificate }}
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-postcard"></i>
-									餐饮服务经营许可证
-								</template>
-								{{ detailedInfo.cateringServiceLicense }}
-							</el-descriptions-item>
-						</el-descriptions>
-						<el-form label-position="formLabelPosition" :model="form">
-							<el-row class="autocomplete">
-								<el-col :span="24">
-									<div class="sub-title">审核说明</div>
-									<el-autocomplete class="inline-input" v-model="dataDescrip"
-										v-bind:disabled="disabledDescrip" :fetch-suggestions="querySearch"
-										placeholder="请输入内容" @select="handleSelect">
-									</el-autocomplete>
-								</el-col>
-							</el-row>
-							<p></p>
-							<div class="sub-title">审核结果</div>
-							<el-select v-model="form.choice" placeholder="请选择审核结果" @change="auditSelect">
-								<el-option label="审核驳回" value=false></el-option>
-								<el-option label="审核通过" value=true></el-option>
-							</el-select>
-						</el-form>
-						<template #footer>
-							<span class="dialog-footer">
-								<el-button type="primary" @click="submitClick" size="small">提交</el-button>
-							</span>
-						</template>
-					</el-dialog>
-				</el-card>
-			</el-main>
+						</el-dialog>
+					</el-card>
+				</el-main>
+			</el-container>
 		</el-container>
-	</el-container>
+	</el-scrollbar>
 </template>
 
 <script>
--- src/views/Administrator/Manage.vue
+++ src/views/Administrator/Manage.vue
@@ -1,185 +1,184 @@
 <template>
-	<el-container style="height: 700px">
-		<el-header style="background-color: #2c3e50; color: white">
-			<back-nav />
-		</el-header>
-		<el-container style="height: 100%">
-			<el-aside width="200px" style="background-color: #545c64">
-				<SideNav />
-			</el-aside>
-			<el-main>
-				<el-card class="box-card">
-					<!--所有已上线的酒店列表-->
-					<h3>已上线酒店列表</h3>
-					<!--按酒店名称查询酒店-->
-					<el-row>
-						<el-col :span="8">
-							<div style="margin-top: 10px">
-								<p style="font-size: small;font-weight: 400;margin-right: 10px;">
-									<i class="el-icon-office-building"></i>
-									查询酒店名称:
-								</p>
-							</div>
-						</el-col>
-						<el-col :span="8">
-							<el-input v-model="searchInput" placeholder="请输入酒店名称" style="margin-top: 2px;"
-								suffix-icon="el-icon-search"></el-input>
-						</el-col>
-						<el-col :span="3"></el-col>
-						<el-button icon="el-icon-search" size="small" type="primary" plain @click="searchHotelName">搜索
-						</el-button>
-					</el-row>
-					<!--已上线酒店列表-->
-					<el-table :data="tableData" border style="width: 100%;margin-top: 10px">
-						<el-table-column prop="hotelID" label="酒店ID" width="120">
-						</el-table-column>
-						<el-table-column prop="hotelName" label="酒店名" width="120">
-						</el-table-column>
-						<el-table-column prop="hotelProvince" label="省份" width="120">
-						</el-table-column>
-						<el-table-column prop="hotelCity" label="城市" width="120">
-						</el-table-column>
-						<el-table-column prop="hotelRegion" label="区" width="120">
-						</el-table-column>
-						<el-table-column prop="hotelAddress" label="地址" width="200">
-						</el-table-column>
-						<!--可筛选配图不全的酒店-->
-						<el-table-column prop="hotelHavePicture" label="配图" width="120"
-							:filters="[{ text: '齐全', value: '齐全' }, { text: '不全', value: '不全' }]"
-							:filter-method="filterTag">
-						</el-table-column>
-						<el-table-column fixed="right" label="详情" width="100">
-							<template #default="scope">
-								<el-button @click="handleClick(scope.row)" type="text" size="small">查看
-								</el-button>
-								<el-button @click="deleteClick(index)" type="text" size="small">删除
-								</el-button>
+	<el-scrollbar height="600px">
+		<el-container style="height: 700px">
+			<el-header style="background-color: #2c3e50; color: white">
+				<back-nav />
+			</el-header>
+			<el-container style="height: 100%">
+				<el-aside width="200px" style="background-color: #545c64">
+					<SideNav />
+				</el-aside>
+				<el-main>
+					<el-card class="box-card">
+						<!--所有已上线的酒店列表-->
+						<h3>已上线酒店列表</h3>
+						<!--按酒店名称查询酒店-->
+						<el-row justify="center">
+							<el-col :span="12">
+								<el-input v-model="searchInput" placeholder="输入酒店名称进行查询" style="margin-top: 2px;" prefix-icon="el-icon-office-building">
+									<template #append>
+										<el-button icon="el-icon-search" @click="searchHotelName"></el-button>
+									</template>
+								</el-input>
+							</el-col>
+						</el-row>
+						<!--已上线酒店列表-->
+						<el-row justify="center">
+							<el-table :data="tableData" border style="width: 90%;margin-top: 10px">
+								<el-table-column prop="hotelID" label="酒店ID" width="120">
+								</el-table-column>
+								<el-table-column prop="hotelName" label="酒店名" width="120">
+								</el-table-column>
+								<el-table-column prop="hotelProvince" label="省份" width="80">
+								</el-table-column>
+								<el-table-column prop="hotelCity" label="城市" width="80">
+								</el-table-column>
+								<el-table-column prop="hotelRegion" label="区" width="80">
+								</el-table-column>
+								<el-table-column prop="hotelAddress" label="地址" width="250">
+								</el-table-column>
+								<!--可筛选配图不全的酒店-->
+								<el-table-column prop="hotelHavePicture" label="配图" width="80"
+									:filters="[{ text: '齐全', value: '齐全' }, { text: '不全', value: '不全' }]"
+									:filter-method="filterTag">
+								</el-table-column>
+								<el-table-column fixed="right" label="详情" width="100" align="center">
+									<template #default="scope">
+										<el-button @click="handleClick(scope.row)" type="text" size="small">查看
+										</el-button>
+										<el-button @click="deleteClick(index)" type="text" size="small">删除
+										</el-button>
+									</template>
+								</el-table-column>
+							</el-table>
+						</el-row>
+						<!--酒店详情对话框-->
+						<el-dialog title="酒店详情" v-model="dialogDetails" width="80%">
+							<!--酒店信息描述列表-->
+							<p style="font-size: medium;font-weight: 600;" align="left">酒店信息</p>
+							<el-descriptions class="margin-top" :column="4" :size="size" border>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-user-solid"></i>
+										酒店ID
+									</template>
+									{{detailedInfo.hotelID}}
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-user"></i>
+										酒店名
+									</template>
+									{{detailedInfo.hotelName}}
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-star-off"></i>
+										星级
+									</template>
+									<el-rate v-model="detailedInfo.starLevel" disabled text-color="#ff9900"
+										disabled-void-color="white">
+									</el-rate>
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-medal"></i>
+										评分
+									</template>
+									<el-rate v-model="detailedInfo.score" disabled show-score text-color="#99A9BF"
+										score-template="{value}"
+										:icon-classes="['iconfont icon-chaping', 'iconfont icon-zhongping', 'iconfont icon-haoping']"
+										disabled-void-color="white" :colors="['#99A9BF', '#F7BA2A', '#FF9900']">
+									</el-rate>
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-map-location"></i>
+										省
+									</template>
+									{{detailedInfo.hotelProvince}}
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-location-information"></i>
+										市
+									</template>
+									{{ detailedInfo.hotelCity }}
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-location-outline"></i>
+										区
+									</template>
+									{{ detailedInfo.hotelRegion }}
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-place"></i>
+										详细地址
+									</template>
+									{{ detailedInfo.hotelAddress }}
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-postcard"></i>
+										卫生许可证
+									</template>
+									{{ detailedInfo.healthLicense }}
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-postcard"></i>
+										消防检查合格证
+									</template>
+									{{ detailedInfo.fireInspectionCertificate }}
+								</el-descriptions-item>
+								<el-descriptions-item>
+									<template #label>
+										<i class="el-icon-postcard"></i>
+										餐饮服务经营许可证
+									</template>
+									{{ detailedInfo.cateringServiceLicense }}
+								</el-descriptions-item>
+							</el-descriptions>
+							<!--酒店图片，可上传-->
+							<p style="font-size: medium;font-weight: 600;" align="left">
+								酒店图片</p>
+							<el-upload class="avatar-uploader" action="https://jsonplaceholder.typicode.com/posts/"
+								align="left" :show-file-list="false" :on-success="hotelHandleAvatarSuccess"
+								:before-upload="beforeHotelAvatarUpload">
+								<img v-if="hotelImageUrl" :src="hotelImageUrl" class="avatar">
+								<i v-else class="el-icon-picture-outline" style="font-size: 600%;"></i>
+							</el-upload>
+							<p align="left">(点击上方上传图片，支持的图片格式为jpg、jpeg、png) </p>
+							<!--酒店房型图片，可以通过选择框选择不同房型，并上传图片-->
+							<p style="font-size: medium;font-weight: 600;" align="left">
+								房型图片 </p>
+							<p align="left">
+								<el-select v-model="roomTypeSelect" placeholder="请选择房型" @change="selectRoom">
+									<el-option v-for="item in roomTypeOptions" :key="item.value" :label="item.label"
+										:value="item.value">
+									</el-option>
+								</el-select>
+							</p>
+							<el-upload class="avatar-uploader" action="https://jsonplaceholder.typicode.com/posts/"
+								align="left" :show-file-list="false" :on-success="roomHandleAvatarSuccess"
+								:before-upload="beforeRoomAvatarUpload">
+								<img v-if="roomImageUrl" :src="roomImageUrl" class="avatar">
+								<i v-else class="el-icon-picture-outline" style="font-size: 600%;"></i>
+							</el-upload>
+							<p align="left">(点击上方上传图片，支持的图片格式为jpg、jpeg、png)</p>
+							<template #footer>
+								<span class="dialog-footer">
+									<el-button type="primary" @click="backClick" size="medium">返回</el-button>
+								</span>
 							</template>
-						</el-table-column>
-					</el-table>
-					<!--酒店详情对话框-->
-					<el-dialog title="酒店详情" v-model="dialogDetails" width="80%">
-						<!--酒店信息描述列表-->
-						<p style="font-size: medium;font-weight: 600;" align="left">酒店信息</p>
-						<el-descriptions class="margin-top" :column="4" :size="size" border>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-user-solid"></i>
-									酒店ID
-								</template>
-								{{detailedInfo.hotelID}}
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-user"></i>
-									酒店名
-								</template>
-								{{detailedInfo.hotelName}}
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-star-off"></i>
-									星级
-								</template>
-								{{detailedInfo.starLevel}}
-								<i class="el-icon-star-on" style="color:#191a1d;font-size: large;"></i>
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-medal"></i>
-									评分
-								</template>
-								<el-rate v-model="hotelScore" disabled show-score text-color="#ff9900"
-									score-template="{value}">
-								</el-rate>
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-map-location"></i>
-									省
-								</template>
-								{{detailedInfo.hotelProvince}}
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-location-information"></i>
-									市
-								</template>
-								{{ detailedInfo.hotelCity }}
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-location-outline"></i>
-									区
-								</template>
-								{{ detailedInfo.hotelRegion }}
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-place"></i>
-									详细地址
-								</template>
-								{{ detailedInfo.hotelAddress }}
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-postcard"></i>
-									卫生许可证
-								</template>
-								{{ detailedInfo.healthLicense }}
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-postcard"></i>
-									消防检查合格证
-								</template>
-								{{ detailedInfo.fireInspectionCertificate }}
-							</el-descriptions-item>
-							<el-descriptions-item>
-								<template #label>
-									<i class="el-icon-postcard"></i>
-									餐饮服务经营许可证
-								</template>
-								{{ detailedInfo.cateringServiceLicense }}
-							</el-descriptions-item>
-						</el-descriptions>
-						<!--酒店图片，可上传-->
-						<p style="font-size: medium;font-weight: 600;" align="left">
-							酒店图片</p>
-						<el-upload class="avatar-uploader" action="https://jsonplaceholder.typicode.com/posts/"
-							align="left" :show-file-list="false" :on-success="hotelHandleAvatarSuccess"
-							:before-upload="beforeHotelAvatarUpload">
-							<img v-if="hotelImageUrl" :src="hotelImageUrl" class="avatar">
-							<i v-else class="el-icon-picture-outline" style="font-size: 600%;"></i>
-						</el-upload>
-						<p align="left">(点击上方上传图片，支持的图片格式为jpg、jpeg、png) </p>
-						<!--酒店房型图片，可以通过选择框选择不同房型，并上传图片-->
-						<p style="font-size: medium;font-weight: 600;" align="left">
-							房型图片 </p>
-						<p align="left">
-							<el-select v-model="roomTypeSelect" placeholder="请选择房型" @change="selectRoom">
-								<el-option v-for="item in roomTypeOptions" :key="item.value" :label="item.label"
-									:value="item.value">
-								</el-option>
-							</el-select>
-						</p>
-						<el-upload class="avatar-uploader" action="https://jsonplaceholder.typicode.com/posts/"
-							align="left" :show-file-list="false" :on-success="roomHandleAvatarSuccess"
-							:before-upload="beforeRoomAvatarUpload">
-							<img v-if="roomImageUrl" :src="roomImageUrl" class="avatar">
-							<i v-else class="el-icon-picture-outline" style="font-size: 600%;"></i>
-						</el-upload>
-						<p align="left">(点击上方上传图片，支持的图片格式为jpg、jpeg、png)</p>
-						<template #footer>
-							<span class="dialog-footer">
-								<el-button type="primary" @click="backClick" size="medium">返回</el-button>
-							</span>
-						</template>
-					</el-dialog>
-				</el-card>
-			</el-main>
+						</el-dialog>
+					</el-card>
+				</el-main>
+			</el-container>
 		</el-container>
-	</el-container>
+	</el-scrollbar>
 </template>
 
 <script>
@@ -399,7 +398,7 @@
 				//给detailedInfo传入该行酒店的详细信息
 				this.detailedInfo = row;
 				//评分用Rate组件，单独传值
-				this.hotelScore = this.detailedInfo.score;
+				//this.hotelScore = this.detailedInfo.score;
 
 				console.log('选择了', this.detailedInfo);
 
