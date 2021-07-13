017632c99cb8c6906a86c882c8f8cd2689dfa746
Linzhou Li <LinzhouLi@outlook.com>
2021-07-13 09:03:02 +0800

    修改百度api的url前缀

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,18 @@
 module.exports = {
     devServer: {
         proxy: { // 为天气接口配置代理，解决跨域
-        '/proxy/': {
-          target: 'http://api.map.baidu.com', //百度地图接口地址
-          changeOrigin: true, // 跨域访问设置，true代表跨域
-          pathRewrite: { // 路径改写规则
-            '^/proxy': '' // 以/proxy/为开头的改写为''
+            '/baidu/': {
+                target: 'http://api.map.baidu.com', //百度地图接口地址
+                changeOrigin: true, // 跨域访问设置，true代表跨域
+                pathRewrite: { // 路径改写规则
+                    '^/baidu': '' // 以/proxy/为开头的改写为''
+                }
+            },
+            '/zhunar/': {
+                target: 'https://121.196.223.20:5001', //百度地图接口地址
+                changeOrigin: true, // 跨域访问设置，true代表跨域
+                pathRewrite: { // 路径改写规则
+                    '^/zhunar': '' // 以/proxy/为开头的改写为''
                 }
             }
         }
