d45cc53b443724298908f5f5294a8f53e942e028
20Yukicode <76719980+20Yukicode@users.noreply.github.com>
2021-07-14 13:53:41 +0800

    Add files via upload

--- src/views/Customer/Order.vue
+++ src/views/Customer/Order.vue
@@ -1,11 +1,5 @@
 <template>
   <el-container>
-    <div style="
-        background-image: url(//s2.hdslb.com/bfs/static/blive/blfe-dynamic-web/static/img/background.bc725153.png);
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-        height: 100%;
-        background-size:100% 100%;">
     <el-header style="padding: 0">
       <TopNav />
     </el-header>
@@ -19,8 +13,8 @@
         <order-detail ref="orderdetail"></order-detail>
 
         <el-tabs v-model="activeName" @tab-click="showTable">
-          <el-tab-pane label="正在进行中" name="second"></el-tab-pane>
-          <el-tab-pane label="历史订单" name="first"></el-tab-pane>
+          <el-tab-pane label="正在进行中" name="first"></el-tab-pane>
+          <el-tab-pane label="历史订单" name="second"></el-tab-pane>
           <el-tab-pane label="已退订" name="third"></el-tab-pane>
         </el-tabs>
         <el-row justify="begin">
@@ -33,7 +27,7 @@
                 end-placeholder="结束日期"
                 format="YYYY 年 MM 月 DD 日"
                 @change="show"
-                v-if="this.activeName != 'second'"
+                v-if="this.activeName != 'first'"
               ></el-date-picker>
             </div>
           </el-col>
@@ -93,7 +87,7 @@
                 type="primary"
                 size="min"
                 v-show="
-                  this.activeName == 'first' || this.activeName == 'third'
+                  this.activeName == 'second' || this.activeName == 'third'
                 "
                 >添加评论</el-button
               >
@@ -108,7 +102,7 @@
                 size="min"
                 type="danger"
                 @click="leaveorder(scope.$index)"
-                v-show="this.activeName === 'second'"
+                v-show="this.activeName === 'first'"
                 >退订</el-button
               >
               <el-button
@@ -116,19 +110,24 @@
                 type="danger"
                 @click="deleteorder(scope.$index)"
                 v-show="
-                  this.activeName == 'first' || this.activeName == 'third'
+                  this.activeName == 'second' || this.activeName == 'third'
                 "
                 >删除</el-button
               >
             </template>
           </el-table-column>
         </el-table>
-        <el-dialog
-          title="评论"
-          v-model="showEva"
-          width="500px"
-          @close="closeEva"
-        >
+        <el-dialog title="订单评价" v-model="showEva" width="500px">
+          <p
+            style="text-align: left"
+            v-if="!!this.orderTemp[this.index].eday_time"
+          >
+            上次评价时间:{{
+              orderTemp[index].eday_time.split("T")[0] +
+              " " +
+              orderTemp[index].eday_time.split("T")[1]
+            }}
+          </p>
           <el-col :span="18">
             <el-rate v-model="this.orderTemp[this.index].star_level" show-text>
             </el-rate>
@@ -175,7 +174,7 @@
               :column="1"
               border="true"
             >
-              <el-descriptions-item label="入离时间">
+              <el-descriptions-item label="入离时间" width="120px">
                 <span>
                   {{ this.orderTemp[this.index].start_date }}后入住
                   <br />
@@ -196,11 +195,15 @@
                   <el-col :span="20">
                     {{ this.orderTemp[this.index].myname }}
                     <br />
-                    ({{ this.orderTemp[this.index].location }})
+                    <span>地址:{{ this.orderTemp[this.index].location }}</span>
                   </el-col>
                   <!-- 这里调用酒店详情路由 -->
                   <el-col :span="4" style="align: right">
-                    <el-button right="0" size="small" @click="reserve"
+                    <el-button
+                      right="0"
+                      type="text"
+                      style="margin-top: 0px"
+                      @click="reserve"
                       >酒店详情</el-button
                     >
                   </el-col>
@@ -212,9 +215,17 @@
                   this.orderTemp[index].room_quantity
                 }}
               </el-descriptions-item>
-              <el-descriptions-item label="其他">
-                <!-- <p>星级{{this.orderTemp[index].star_level}}</p>
-                <p>评价信息{{this.orderTemp[index].details}}</p> -->
+              <el-descriptions-item label="评价">
+                <div v-if="!!this.orderTemp[index].eday_time">
+                  <el-rate
+                    v-model="this.orderTemp[index].star_level"
+                    disabled
+                  ></el-rate>
+                  {{ this.orderTemp[index].details }}
+                </div>
+                <div v-else>
+                  <p>您还没有去评价订单哦</p>
+                </div>
               </el-descriptions-item>
             </el-descriptions>
             <el-descriptions
@@ -240,12 +251,6 @@
         </el-drawer>
       </el-card>
     </el-main>
-    </div>
-    <el-footer style="background-color: #f6f9fa; height:100%">
-    <p style="margin:0px; padding:30px 0px 40px 0px">
-      Copyright ©2021 住哪儿-酒店预定平台
-    </p>
-  </el-footer>
   </el-container>
 </template>
 
@@ -263,36 +268,36 @@ export default {
       time1: "",
       time2: "",
       // 说明绑定的是历史订单还是其他的什么
-      activeName: "second",
+      activeName: "first",
       dataRange: null,
       index: -1,
       dialogVisible: false,
       showEva: false,
       order: [
-        {
-          customer_name: "nzh",
-          order_id: "123",
-          day_time: new Date("2021-04-06 19:45"),
-          url: "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg",
-          myname: "上海田园佳美酒店",
-          start_date: "2017-08-01 18:56:38",
-          end_date: "2017/08/28 04:56:38",
-          order_money: 828,
-          order_status: "已退订",
-          location: "曹安公路4800号",
-          room_type: "大床房",
-          phone_num: 12345,
-          timeLength: "",
-          star_level: "",
-          details: "",
-          eday_time: "",
-        },
+        // {
+        //   customer_name: "nzh",
+        //   order_id: "123",
+        //   day_time: new Date("2021-04-06 19:45"),
+        //   url: "https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg",
+        //   myname: "上海田园佳美酒店",
+        //   start_date: "2017-08-01 18:56:38",
+        //   end_date: "2017/08/28 04:56:38",
+        //   order_money: 828,
+        //   order_status: "已退订",
+        //   location: "曹安公路4800号",
+        //   room_type: "大床房",
+        //   phone_num: 12345,
+        //   timeLength: "",
+        //   star_level: "",
+        //   details: "",
+        //   eday_time: "",
+        // },
       ],
       orderTemp: [], //这里是一个替代的数组
     };
   },
   created() {
-    this.getData();
+    this.showTable();
   },
   methods: {
     /*此处调用api中删除订单的方法*/
@@ -307,24 +312,17 @@ export default {
         .then(() => {
           this.axios
             .delete(
-              "http://121.196.223.20:5000/api/customerorder/delete/" +
+              "/zhunar/api/customerorder/delete/" +
                 this.orderTemp[newindex].order_id
             )
             .then((response) => {
               console.log(response.data);
             });
-          //这里要使得服务器订单对应删除
           this.$message({
             type: "success",
             message: "删除成功!",
           });
-          for (var item of this.order) {
-            if (item.order_id == this.orderTemp[newindex].order_id) {
-              this.orderTemp.splice(newindex, 1);
-              this.order.splice(this.order.indexOf(item), 1);
-              break;
-            }
-          }
+          this.orderTemp.splice(newindex, 1);
         })
         .catch(() => {
           this.$message({
@@ -345,26 +343,16 @@ export default {
         .then(() => {
           this.axios
             .put(
-              "http://121.196.223.20:5000/api/customerorder/cancel/" +
+              "/zhunar/api/customerorder/cancel/" +
                 this.orderTemp[newindex].order_id
             )
-            .then((response) => {
-              console.log(response.data);
-            });
-          for (var item of this.order) {
-            if (item.order_id == this.orderTemp[newindex].order_id) {
-              var temp = item;
-              temp.order_status = "已退订";
-              this.order.push(temp);
+            .then(() => {
               this.orderTemp.splice(newindex, 1);
-              this.order.splice(this.order.indexOf(item), 1);
-              break;
-            }
-          }
-          this.$message({
-            type: "success",
-            message: "退订成功!",
-          });
+              this.$message({
+                type: "success",
+                message: "退订成功!",
+              });
+            });
         })
         .catch(() => {
           this.$message({
@@ -373,24 +361,6 @@ export default {
           });
         });
     },
-    /*closeEva()
-    {
-       this.$confirm("请确认是否退出(您还没有保存评价信息)？", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning",
-      })
-        .then(()=>{
-          this.orderTemp[this.index].star_level=""
-          this.orderTemp[this.index].details=""
-          this.showEva=false;
-        })
-        .catch(() => {
-          this.orderTemp[this.index].star_level=""
-          this.orderTemp[this.index].details=""
-          this.showEva=true;
-        });
-    },*/
     //显示详情
     showDialog(newindex) {
       this.index = newindex;
@@ -399,60 +369,28 @@ export default {
     //展示通过时间选择器筛选订单
     show() {
       if (this.dataRange != null) {
-        this.time1 = this.dataRange[0]//.split('T')[0]
-        this.time2 = this.dataRange[1]//.split('T')[0]
+        this.time1 = this.dataRange[0]; //.split('T')[0]
+        this.time2 = this.dataRange[1]; //.split('T')[0]
       }
       this.showTable();
     },
-    getData() {
+    getData(status) {
       this.a = true;
       this.axios
         .get(
-          "http://121.196.223.20:5000/api/customerorder/gethotel/" +
-            this.c_user_id
+          "/zhunar/api/customerorder/customer/" + this.c_user_id + "/" + status
         )
         .then((response) => {
           this.order = response.data;
           console.log(this.order);
           for (let item of this.order) {
-            item.start_date = item.start_date.split("T")[0].replace(/-/g,'/')
-            item.end_date = item.end_date.split("T")[0].replace(/-/g,'/')
-            item.day_time = item.day_time.split("T")[0].replace(/-/g,'/')
+            item.start_date = item.start_date.split("T")[0].replace(/-/g, "/");
+            item.end_date = item.end_date.split("T")[0].replace(/-/g, "/");
+            item.day_time = item.day_time.split("T")[0].replace(/-/g, "/");
             item.timeLength =
               (new Date(item.end_date) - new Date(item.start_date)) / 86400000;
           }
-          this.a = false;
-          this.showTable();
-        });
-    },
-    showTable() {
-      if (this.activeName == "first") {
-        this.orderTemp.length = 0;
-        for (var i = 0; i < this.order.length; i++) {
-          if (this.order[i].order_status == "已完成") {
-            
-            if (
-              this.dataRange == null ||
-              (new Date(this.order[i].day_time) >= new Date(this.time1) &&
-                new Date(this.order[i].day_time) <= new Date(this.time2))
-            )
-              this.orderTemp.push(this.order[i]);
-          }
-        }
-      } else if (this.activeName == "second") {
-        this.orderTemp.length = 0;
-        this.dataRange = null;
-        for (var j = 0; j < this.order.length; j++) {
-          if (this.order[j].order_status == "进行中") {
-            this.orderTemp.push(this.order[j]);
-          }
-        }
-      } else if (this.activeName == "third") {
-        this.orderTemp.length = 0;
-        
-        for (var k = 0; k < this.order.length; k++) {
-          if (this.order[k].order_status == "已退订") {
-            console.log(new Date(this.order[k].day_time),this.time1,this.time2)
+          for (var k = 0; k < this.order.length; k++) {
             if (
               this.dataRange == null ||
               (new Date(this.order[k].day_time) >= new Date(this.time1) &&
@@ -460,33 +398,54 @@ export default {
             )
               this.orderTemp.push(this.order[k]);
           }
-        }
+          this.a = false;
+        });
+    },
+    showTable() {
+      if (this.activeName == "first") {
+        this.dataRange = null;
+        this.orderTemp.length = 0;
+        this.getData("进行中");
+      } else if (this.activeName == "third") {
+        this.orderTemp.length = 0;
+        this.getData("已退订");
+      } else if(this.activeName=="second")
+      {
+        this.orderTemp.length = 0;
+        this.getData("已完成");
       }
     },
+    //展示评论信息
     showEvaDialog(newindex) {
       this.index = newindex;
-      this.orderTemp[this.index].star_level = 0;
-      this.orderTemp[this.index].details = "";
+      if (!!this.orderTemp[this.index].eday_time && true) {
+        console.log("233");
+      } else {
+        this.orderTemp[this.index].star_level = 0;
+        this.orderTemp[this.index].details = "";
+      }
       this.showEva = true;
     },
+      // 这里把评价信息传入数据库存储
     onSubmit() {
       this.showEva = false;
+      console.log(this.orderTemp[this.index])
       this.axios
         .post(
-          "http://121.196.223.20:5000/api/estimation/add?order_id=" +
+          "/zhunar/api/estimation/add/" +
             this.orderTemp[this.index].order_id +
-            "&details=" +
+            "/" +
             this.orderTemp[this.index].details +
-            "&star_level=" +
+            "/" +
             this.orderTemp[this.index].star_level
         )
-        .then(() => {
+        .then((response) => {
+          this.orderTemp[this.index].eday_time=response.data
           this.$message({
             type: "success",
             message: "提交成功!",
           });
         });
-      // 这里把评价信息传入数据库存储
     },
     reserve() {
       this.$router.push({
@@ -506,7 +465,7 @@ export default {
 }
 #order_status {
   font-size: 25px;
-  font-weight: 600;
+  font-weight: 550;
   color: #000;
 }
 </style>
